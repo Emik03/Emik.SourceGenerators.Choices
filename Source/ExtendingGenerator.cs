d787939d3d07337e8362e88a4ff1915a4f68994a
Emik <emik.lakunishok@gmail.com>
2024-07-12 18:20:47 +0200

    Add XML documentation for the only useful exposed function

--- Source/ExtendingGenerator.cs
+++ Source/ExtendingGenerator.cs
@@ -1,22 +1,53 @@
 // SPDX-License-Identifier: MPL-2.0
 namespace Emik.SourceGenerators.Choices;
 
-/// <summary>The source generator that implements implicit operators.</summary>
+/// <summary>The source generator responsible for generating disjoint union functionality to annotated types.</summary>
 [Generator]
 public sealed class ExtendingGenerator : IIncrementalGenerator
 {
     const int MinimumFields = 2;
 
+    /// <summary>Executes this source generation based on given input.</summary>
+    /// <remarks><para>
+    /// This API is unused within the library, but can be used by other libraries that reference
+    /// this library to integrate this source generator however they wish. Do note that the API
+    /// is subject to change, but the general premise of the function will remain the same.
+    /// </para></remarks>
+    /// <param name="named">The type to generate for.</param>
+    /// <param name="publiclyMutable">
+    /// If <see langword="null"/>, the fields and properties generated will be immutable.
+    /// If <see langword="false"/>, the fields and properties generated will be mutable, but only from within the type.
+    /// If <see langword="true"/>, the fields and properties generated will be publicly mutable.
+    /// </param>
+    /// <param name="polyfillAttributes">
+    /// Determines whether to generate private attributes required to make dot-declaration
+    /// syntax work. Set this to <see langword="true"/> if you have annotated the parameter
+    /// <paramref name="named"/> using the dot-declaration feature; nested class attributes such as
+    /// <c>[Choice.Public.Foo&lt;Bar&gt;.Baz&lt;Qux&gt;]</c> versus <c>[Choice(true, typeof((Bar Foo, Qux Baz)))]</c>.
+    /// </param>
+    /// <param name="members">
+    /// The variants of the parameter <paramref name="named"/>. Variants created from a <see cref="IFieldSymbol"/> or
+    /// <see cref="IPropertySymbol"/> will not create the respective members in the source generation, since this
+    /// indicates that the member already exists, and would cause a compiler error if a second member was generated.
+    /// </param>
+    /// <returns>
+    /// The generated source. This contains the hint name, which indicates the name of the file, and the source,
+    /// representing the contents of said file. This function will return <see langword="null"/> under a few
+    /// circumstances. This includes the parameter <paramref name="named"/> being <see langword="null"/>, being a tuple
+    /// type regardless of if it is a polyfill or not, not being annotated with <c>[Choice]</c> or annotated correctly,
+    /// not being fully <see langword="partial"/> which includes any types that contain the parameter
+    /// <paramref name="named"/>, the parameter <see cref="members"/> being less than 2 elements long.
+    /// </returns>
     [Pure]
     public static GeneratedSource? Transform(
-        INamedTypeSymbol named,
-        bool? publiclyMutable = true,
+        INamedTypeSymbol? named,
+        bool? publiclyMutable = null,
         bool polyfillAttributes = false,
-        params MemberSymbol[] fields
+        params MemberSymbol[] members
     ) =>
-        !named.IsTupleType &&
-        fields.Length >= MinimumFields &&
-        (named, fields.ToSmallList(), publiclyMutable, polyfillAttributes) is var raw &&
+        named is { IsTupleType: false } &&
+        members.Length >= MinimumFields &&
+        (named, members.ToSmallList(), publiclyMutable, polyfillAttributes) is var raw &&
         HasSufficientFields(raw)
             ? ((Scaffolder)raw).Result
             : null;
