e1b1925f5bd9bc2da292e81795a6e31406d68ac5
Emik <emik.lakunishok@gmail.com>
2024-07-12 04:31:39 +0200

    Add example

--- Emik.SourceGenerators.Choices.Generated.Tests/Program.cs
+++ Emik.SourceGenerators.Choices.Generated.Tests/Program.cs
@@ -1,1 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
+using Emik.SourceGenerators.Choices.Generated.Tests;
+
+var a = DotKMModule.OfOther(new Component());
+var b = a.GetUnderlyingValue();
+Console.WriteLine(b);
