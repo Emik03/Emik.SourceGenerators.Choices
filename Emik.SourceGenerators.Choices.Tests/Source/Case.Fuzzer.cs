db06a45773215af68d585de270882f65f6e7d310
Emik <emik.lakunishok@gmail.com>
2025-03-13 20:40:31 +0100

    Rename class to Fuzzer

--- Emik.SourceGenerators.Choices.Tests/Source/Case.Fuzzer.cs
+++ Emik.SourceGenerators.Choices.Tests/Source/Case.Fuzzer.cs
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MPL-2.0
 namespace Emik.SourceGenerators.Choices.Tests;
 
+using Test = (string Structure, string TypeKeyword, INamedTypeSymbol First, INamedTypeSymbol Second, Verify Verify);
+
 public partial class Case
 {
-    public sealed class ExhaustStandardLibrary(ITestOutputHelper output)
+    public sealed class Fuzzer(ITestOutputHelper output)
     {
-        const int UpdateMeEvery = 1000;
-
         const string DotPattern = // language=cs
             """
             [Choice.First<{1}>.Second<{2}>, Obsolete]
@@ -41,6 +41,9 @@ public partial class Case
             }}
             """;
 
+        static IEnumerable<MetadataReference> References { get; } =
+            Net90.References.All.Where(x => x.Display?.Contains("System") is true);
+
         static ImmutableArray<string> Structures { get; } = [DotPattern, Fields, PrimaryConstructors, Properties];
 
         static ImmutableArray<string> TypeKeywords { get; } = ["class", "record", "record struct", "struct"];
@@ -48,7 +51,7 @@ public partial class Case
         static ImmutableArray<INamedTypeSymbol> AccessibleTypes { get; } =
         [
             ..CSharpCompilation.Create("tmp")
-               .AddReferences(Net90.References.All.Where(x => x.Display?.Contains("System") is true))
+               .AddReferences(References)
                .GlobalNamespace
                .GetMembers()
                .SelectMany(GetMembers)
@@ -57,17 +60,18 @@ public partial class Case
                .Where(x => GetBaseTypes(x).All(x => x.DeclaredAccessibility is Accessibility.Public)),
         ];
 
+        // [StressfulFact]
         [Fact]
-        public Task Run1Async() => EnumerateAsync(1);
+        public Task RunSingleAsync() => EnumerateAsync(1);
 
         // [Fact]
-        // public Task Run2Async() => EnumerateAsync(2);
+        // public Task RunDoubleAsync() => EnumerateAsync(2);
 
         // [Fact]
-        // public Task Run4Async() => EnumerateAsync(4);
+        // public Task RunQuadrupleAsync() => EnumerateAsync(4);
 
         // [Fact]
-        // public Task Run8Async() => EnumerateAsync(8);
+        // public Task RunOctupleAsync() => EnumerateAsync(8);
 
         // [Fact]
         // public Task RunAllAsync() => EnumerateAsync(int.MaxValue);
@@ -77,28 +81,28 @@ public partial class Case
             static int FlattenedLength(int source, int size) =>
                 source / size * size * size + source % size * (source % size);
 
-            static IEnumerable<Verify> Query(INamedTypeSymbol[] array) =>
+            static IEnumerable<Test> Query(INamedTypeSymbol[] array) =>
                 from second in array
                 from first in array
                 from typeKeyword in TypeKeywords
                 from structure in Structures
-                select new Verify
+                select (structure, typeKeyword, first, second, new Verify
                 {
                     TestCode = Wrap(string.Format(structure, typeKeyword, first, second)),
                     TestBehaviors = TestBehaviors.SkipGeneratedSourcesCheck,
-                };
+                });
 
-            var i = 0;
-            Verify? fail = null;
             var pass = FlattenedLength(AccessibleTypes.Length, size);
             var length = pass * Structures.Length * TypeKeywords.Length;
             var milestone = pass;
+            Test fail = default;
+            var i = 0;
 
-            async Task RunAsync(Verify x)
+            async Task RunAsync(Test x)
             {
                 try // ReSharper disable once AccessToModifiedClosure
                 {
-                    await x.RunAsync();
+                    await x.Verify.RunAsync();
                     Interlocked.Increment(ref i);
                 }
                 catch (Exception)
@@ -111,6 +115,7 @@ public partial class Case
             var tasks = AccessibleTypes.Chunk(size)
                .SelectMany(Query)
                .Chunk(Environment.ProcessorCount)
+               .Skip(1455)
                .Select(x => Task.WhenAll(x.Select(RunAsync)));
 
             output.WriteLine($"Running {length} micro-tests.");
@@ -130,9 +135,54 @@ public partial class Case
             }
             catch (Exception e)
             {
-                var source = Display(fail?.TestState.Sources);
-                throw new InvalidOperationException($"// Micro-test {i}/{length} caused invalid codegen:\n{source}", e);
+                Throw(e, fail, i, length);
             }
+        }
+
+        static void Throw(Exception e, Test fail, int i, int length)
+        {
+            var (structure, typeKeyword, first, second, verify) = fail;
+
+            bool MatchesKind(ISymbol x, int i) =>
+                structure switch
+                {
+                    Fields => x is IFieldSymbol,
+                    Properties => x is IPropertySymbol,
+                    PrimaryConstructors => x is IParameterSymbol,
+                    _ => i < 2,
+                };
+
+            var syntaxTrees = CSharpSyntaxTree.ParseText(Wrap(string.Format(structure, typeKeyword, first, second)));
+
+            var type = CSharpCompilation.Create("tmp", [syntaxTrees])
+               .AddReferences(References)
+               .GlobalNamespace
+               .GetNamespaceMembers()
+               .Single(x => x.Name is nameof(Emik))
+               .GetNamespaceMembers()
+               .Single(x => x.Name is nameof(SourceGenerators))
+               .GetNamespaceMembers()
+               .Single(x => x.Name is nameof(Choices))
+               .GetNamespaceMembers()
+               .Single()
+               .GetTypeMembers()
+               .Single();
+
+            var members = type.GetMembers()
+               .Where(MatchesKind)
+               .Select(MemberSymbol.From)
+               .Where(x => x is not null) // ReSharper disable once NullableWarningSuppressionIsUsed
+               .Select(x => x!.Value)
+               .ToArray() is { Length: >= 2 } arr
+                ? arr
+                : [new(first, nameof(first)), new(second, nameof(second))];
+
+            var source = ExtendingGenerator.Transform(type, members: members) is var (hintName, contents)
+                ? $"{hintName}:\n{contents}"
+                : "Failed to display source.";
+
+            InvalidOperationException display = new($"{Display(verify.TestState.Sources)}\n// {source}");
+            throw new AggregateException($"Micro-test {i}/{length} caused invalid codegen.", e, display);
         }
 
         static bool IsNonGenericInstance(INamedTypeSymbol? x) =>
